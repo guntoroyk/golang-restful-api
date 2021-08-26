d175585877ca32eef2a19861a90ea2ddef3fae80
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-08-26 16:21:54 +0700

    feat: create unit test for category_service_impl with mock of category repo

--- service/category_service_impl_test.go
+++ service/category_service_impl_test.go
@@ -1,10 +1,17 @@
 package service
 
 import (
+	"context"
+	"fmt"
+	"golang-restful-api/model/domain"
+	"golang-restful-api/model/web"
+
 	//"context"
 	//"github.com/golang/mock/gomock"
 	//"golang-restful-api/model/domain"
 	//"golang-restful-api/repository/mocks"
+	"github.com/golang/mock/gomock"
+	"golang-restful-api/repository/mocks"
 	"testing"
 )
 
@@ -13,10 +20,134 @@ func TestCreateCategory(t *testing.T) {
 }
 
 func TestCategoryServiceImpl_FindAll(t *testing.T) {
-	//mockCtrl := gomock.NewController(t)
-	//defer mockCtrl.Finish()
-	//mockCategoryRepo := mocks.NewMockCategoryRepository(mockCtrl)
-	//mockCategoryRepo.EXPECT().FindAll(context.Background())
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockCategoryRepo := mocks.NewMockCategoryRepository(mockCtrl)
+	mockCategoryRepo.EXPECT().FindAll(context.Background()).Return([]domain.Category{{
+		Id: 1,
+		Name: "Computer",
+	}, {
+		Id: 2,
+		Name: "Gadget",
+	}})
+
+	categoryService := NewCategoryService(mockCategoryRepo)
+
+	categories := categoryService.FindAll(context.Background())
+
+	fmt.Println("categories", categories)
+	got := len(categories)
+	want := 2
+	if got != want {
+		t.Errorf("got %d want %d", got, want )
+	}
+}
+
+func TestCategoryServiceImpl_FindById(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockCategoryRepo := mocks.NewMockCategoryRepository(mockCtrl)
+
+	category := domain.Category{
+		Id: 1,
+		Name: "Computer",
+	}
+
+	mockCategoryRepo.EXPECT().FindById(context.Background(), category.Id).Return(domain.Category{Id: 1, Name: "Computer"}, nil)
+
+	categoryService := NewCategoryService(mockCategoryRepo)
+
+	categoryResult := categoryService.FindById(context.Background(), 1)
+
+	fmt.Println("category", categoryResult)
+	got := categoryResult
+	want := web.CategoryResponse{Id: 1, Name: "Computer"}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want )
+	}
+}
+
+func TestCategoryServiceImpl_Create(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockCategoryRepo := mocks.NewMockCategoryRepository(mockCtrl)
+
+	category := domain.Category{
+		Name: "Computer",
+	}
+	mockCategoryRepo.EXPECT().Save(context.Background(), category).Return(domain.Category{Id: 1, Name: "Computer"})
+	categoryService := NewCategoryService(mockCategoryRepo)
+
+	request := web.CategoryCreateRequest{Name: "Computer"}
+
+	categoryResult := categoryService.Create(context.Background(), request)
+	fmt.Println("CategoryResult", categoryResult)
+
+	got := categoryResult
+	want := web.CategoryResponse{Id: 1, Name: "Computer"}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want )
+	}
+}
+
+func TestCategoryServiceImpl_Delete(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockCategoryRepo := mocks.NewMockCategoryRepository(mockCtrl)
+
+	category := domain.Category{
+		Id: 1,
+		Name: "Computer",
+	}
+
+	mockCategoryRepo.EXPECT().FindById(context.Background(), category.Id).Return(domain.Category{Id: 1, Name: "Computer"}, nil)
+	mockCategoryRepo.EXPECT().Delete(context.Background(), category)
+
+	categoryService := NewCategoryService(mockCategoryRepo)
+
+	categoryService.Delete(context.Background(), 1)
+
+	//fmt.Println("category", categoryResult)
+	//got := categoryResult
+	//want := web.CategoryResponse{Id: 1, Name: "Computer"}
 	//
-	//categoryService := NewCategoryService()
+	//if got != want {
+	//	t.Errorf("got %q want %q", got, want )
+	//}
+}
+
+func TestCategoryServiceImpl_Update(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockCategoryRepo := mocks.NewMockCategoryRepository(mockCtrl)
+
+	oldCategory := domain.Category{
+		Id: 1,
+		Name: "Computer",
+	}
+
+	mockCategoryRepo.EXPECT().FindById(context.Background(), 1).Return(oldCategory, nil)
+
+	newCategory := domain.Category{
+		Id: 1,
+		Name: "Gadget",
+	}
+
+	mockCategoryRepo.EXPECT().Update(context.Background(), newCategory).Return(newCategory)
+
+	categoryService := NewCategoryService(mockCategoryRepo)
+
+	request := web.CategoryUpdateRequest{Id: 1, Name: "Gadget"}
+
+	categoryResult := categoryService.Update(context.Background(), request)
+	fmt.Println("CategoryResult", categoryResult)
+
+	got := categoryResult
+	want := web.CategoryResponse{Id: 1, Name: "Gadget"}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want )
+	}
 }
